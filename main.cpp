e4d56d8ee7f623cb4bb8ce4423a447a8ac904a29
Johannes Moe <arenor.moe@gmail.com>
2018-10-07 16:10:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
   if (argc > 1) {
     cout << "argv[1] = " << argv[1] << endl;
   } else {
-        cout << "Terminal Error: No initial file given.";
+    cout << "Terminal Error: No initial file given.";
     return -1;
   }
   ifstream mainfile(argv[1]); //open the file
@@ -18,16 +18,14 @@ int main(int argc, char* argv[]) {
     //We can now forward file to methods
 
     /*
-    cout << "File is now open!\nContains:\n";
-    string line = "";
-    while (getline(infile, line)){
+      cout << "File is now open!\nContains:\n";
+      string line = "";
+      while (getline(infile, line)){
         cout << line << '\n';
-    }
+      }
     */
   } else {
     cout << "Terminal Error: Initial file could not be Opened";
   }
   return 0;
-}
-
-// Thanks in parts to Nick Evan on s(Daniweb)
+}// Thanks in parts to Nick Evan on s(Daniweb)
