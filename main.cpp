0bfd5615a7e6923b8ac4119de2edfb243aac3298
Johannes Moe <arenor.moe@gmail.com>
2018-10-07 15:30:54 +0200

    Opening of initia file, main method.
    Needs furthe changes. This change is to start the initial processes.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,33 @@
-int main() {
+// basic file operations
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  if (argc > 1) {
+    cout << "argv[1] = " << argv[1] << endl;
+  } else {
+        cout << "Terminal Error: No initial file given.";
+    return -1;
+  }
+  ifstream mainfile(argv[1]); //open the file
+
+  if (mainfile.is_open() && mainfile.good()) {
+    //File aquired
+    //We can now forward file to methods
+
+    /*
+    cout << "File is now open!\nContains:\n";
+    string line = "";
+    while (getline(infile, line)){
+        cout << line << '\n';
+    }
+    */
+  } else {
+    cout << "Terminal Error: Initial file could not be Opened";
+  }
+  return 0;
 }
+
+// Thanks in parts to Nick Evan on s(Daniweb)
